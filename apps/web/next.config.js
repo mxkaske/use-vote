cb4f152d8199ade6aedac288da50ef538951729d
Maximilian Kaske <maximilian@kaske.org>
2022-10-12 20:57:12 +0200

    fix: remove compose-plugin

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,12 @@
 const withTM = require("next-transpile-modules")(["@use-vote/next"]);
-const withPlugins = require("next-compose-plugins");
 const cl = require("next-contentlayer");
 
 const nextConfig = { reactStrictMode: true };
 
-module.exports = withPlugins([withTM, cl.withContentlayer({})], nextConfig);
+module.exports = (_phase, { defaultConfig }) => {
+  const plugins = [cl.withContentlayer, withTM];
+  return plugins.reduce((acc, plugin) => plugin(acc), {
+    ...defaultConfig,
+    ...nextConfig,
+  });
+};
